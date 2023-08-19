cb2357c1ce2a19f01bf980e57e9d4be973c9d3ed
GianlucaCobucci <gianlucacobucci330@gmail.com>
2023-08-19 15:45:11 +0200

    added dimmering of background color

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const root = document.documentElement;
 //SIDEBAR
 const menuItems = document.querySelectorAll(".menu-item")
 //MESSAGGI
@@ -8,9 +9,16 @@ const messageSearch = document.querySelector("#message-search");
 //THEME CUSTOM
 const theme = document.querySelector("#theme");
 const themeModal = document.querySelector(".customize-theme");
-//CHANGE COLOR 
+//CHANGE THEME COLOR 
 const colorPalette = document.querySelectorAll(".choose-color span");
-let primaryHue = 252; 
+let primaryHue = 252;
+//CHANGE BACKGOUND COLOR 
+const bg1 = document.querySelector(".bg-1");
+const bg2 = document.querySelector(".bg-2");
+const bg3 = document.querySelector(".bg-3");
+let lightColorLightness = "95%";
+let whiteColorLightness = "100%";
+let darkColorLightness = "17%";
 
 /* SIDEBAR */
 //rimuovi active da tutti gli items della sidebar
@@ -19,7 +27,6 @@ const changeActiveItem = () => {
         item.classList.remove("active");
     })
 }
-
 //aggiungi active (striscia viola) a tutti gli items della sidebar
 menuItems.forEach(item => {
     item.addEventListener("click", () => {
@@ -43,7 +50,6 @@ messageSearch.addEventListener("keyup", () => {
         chat.style.display = name.includes(value) ? "flex" : "none";
     });
 });
-
 //sottolinea messaggi quando menu item è cliccato
 messagesNotification.addEventListener("click", () => {
     messages.style.boxShadow = "0 0 1rem var(--color-primary)";
@@ -53,19 +59,18 @@ messagesNotification.addEventListener("click", () => {
     }, 1000);
 })
 
-/* THEME CUSTOMIZATION */
+/* THEME CUSTOM */
 //apri e chiudi modale
 theme.addEventListener("click", () => {
     themeModal.style.display = "grid";
 });
-
 themeModal.addEventListener("click", (event) => {
     if (event.target.classList.contains("customize-theme")) {
         themeModal.style.display = "none";
     }
 });
 
-/* CHANGE COLOR */
+/* CHANGE THEME COLOR */
 //cambia colore principale
 colorPalette.forEach(color => {
     color.addEventListener("click", () => {
@@ -85,10 +90,44 @@ colorPalette.forEach(color => {
         document.documentElement.style.setProperty("--primary-color-hue", primaryHue);
     });
 });
-
 //togli active 
 const changeActiveColorClass = () => {
     colorPalette.forEach(colorPicker => {
         colorPicker.classList.remove("active");
     })
 }
+
+/* CHANGE BACKGOUND COLOR  */
+//cambia colore backgound
+const changeBG = () => {
+    root.style.setProperty("--light-color-lightness", lightColorLightness)
+    root.style.setProperty("--white-color-lightness", whiteColorLightness)
+    root.style.setProperty("--dark-color-lightness", darkColorLightness)
+}
+const setActiveBackground = (activeBG) => {
+    bg1.classList.remove("active");
+    bg2.classList.remove("active");
+    bg3.classList.remove("active");
+    activeBG.classList.add("active");
+};
+bg1.addEventListener("click", () => {
+    lightColorLightness = "95%";
+    whiteColorLightness = "100%";
+    darkColorLightness = "17%";
+    setActiveBackground(bg1);
+    changeBG();
+});
+bg2.addEventListener("click", () => {
+    lightColorLightness = "15%";
+    whiteColorLightness = "20%";
+    darkColorLightness = "95%";
+    setActiveBackground(bg2);
+    changeBG();
+});
+bg3.addEventListener("click", () => {
+    lightColorLightness = "0%";
+    whiteColorLightness = "10%";
+    darkColorLightness = "95%";
+    setActiveBackground(bg3);
+    changeBG();
+});
